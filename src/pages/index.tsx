9525c76f0d2f4074dbe038f059be030dd1b3a396
Mário César Silva <mcgato33@hotmail.com>
2021-04-21 05:39:05 -0100

    getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,14 @@ export default function Home() {
     </div>
   )
 }
-//  export async function getStaticProps() {
-//    const response = await fetch('http://localhost:3333/episodes');
-//    const data = await response.json()
+ export async function getStaticProps() {
+   const response = await fetch('http://localhost:3333/episodes');
+   const data = await response.json()
 
-//    return {
-//      props: {
-//        episodes: data,
-//      },
-//      revalidate: 60 * 60 * 8,
-//    }
-//  }
+   return {
+     props: {
+       episodes: data,
+     },
+     revalidate: 60 * 60 * 8,
+   }
+ }
